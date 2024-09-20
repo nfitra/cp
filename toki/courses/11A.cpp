5bd9f5dfbed3d9c95f2f69da83f73359cf1b98c1
Nanda Fitra Tsalatsa <01123120039@kpndom.com>
2024-09-20 16:46:46 +0700

    updates

---
+++ toki/courses/11A.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <limits>
+#include <cstring>
+#include <cmath>
+#include <unordered_map>
+ 
+#define fast ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define lcase(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define ucase(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+#define ll long long
+#define ull unsigned long long
+#define sz(s) sizeof(s) / sizeof(s[0])
+#define all(s) x.begin(), x.end()
+#define loop(n) for (int i = 0; i < n; i++)
+#define arrsort(arr) sort(arr, arr + sizeof(arr) / sizeof(arr[0]))
+ 
+using namespace std;
+ 
+int main()
+{
+    fast;
+
+    int N;
+    cin >> N;
+
+    int arr[N];
+    int total = 0;
+    for(int i=0; i<N; i++)
+    {
+        cin >> arr[i];
+        total += arr[i];
+    }
+
+    for(int i=0; i<N; i++)
+    {
+        cout << total - arr[i] << endl;
+    }
+}
